2df6fc903fd3de974ebd903603cd231413544e6f
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-12-04 03:31:37 -0800

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
-def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0] if length > 0 else None
-    return length, first
+#!/usr/bin/python3
 
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
