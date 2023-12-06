397dde3ecd1c870522687ffcfc72f2b4ad97549a
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-12-06 23:38:00 +0000

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
-nt(roman_string: str):
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
     if roman_string is None or type(roman_string) != str:
         return 0
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -12,4 +13,3 @@ nt(roman_string: str):
             rep -= numbers[y]
 
     return rep
-
