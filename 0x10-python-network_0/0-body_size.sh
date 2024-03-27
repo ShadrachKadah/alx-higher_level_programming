5072f6694821d8e54874f94c6d12dd17b24749f8
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-03-27 06:30:49 -0700

    1. cURL to the end

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to the URL at $1 and displays the size of the body of the response
-
+# sends a request to the URL at $1 and displays the size of the body of the responese
 curl -s "$1" | wc -c
