3ba7478a6116fec9d217adfadb604ff7acc1fd8d
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-03-28 16:00:42 -0700

    0x11. Python - Network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Usage: ./100-github_commits.py <repository name> <repository owner>
+"""Takes my Github creds (username & password)
+and uses the Github API to display my ID
 """
+
 import sys
 import requests
-
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    token = HTTPBasicAuth(username, password)
+    request = requests.get('https://api.github.com/user', auth=token)
+    print(request.json().get('id'))
